898d0677aa2dfde7e14c9459fecfe434d2abea11
sangeethdvn <123802785+sangeethdvn@users.noreply.github.com>
2025-02-24 14:18:48 +0530

    Add files via upload

---
+++ Collections/Tuple/intro.py
@@ -0,0 +1,21 @@
+# items = (2,4,6,8,8,2,True)              TUPLES ARE PUT IN "()"
+
+# print(items)         #heterogenous, follows order of index (order maintained)
+
+# =================================================================
+
+# index(element)
+# count(element)            ==> to print the number of occurences of element             
+#  ONLY 2 Methods 
+
+# =================================================================
+
+
+# LIST CAN BE MODIFIED BUT        NOT TUPLE 
+
+# tuples cant be modiefied so it should be converted to list 
+
+
+elemets = ("hello", 2, [True, 30,"hair"]) 
+
+print(elemets[2][1]) 
---
+++ Collections/Tuple/sample.py
@@ -0,0 +1,29 @@
+# elemets = ("hello", 2, [True, 30,"hai"])
+
+# (elemets[2].append("Python"))
+
+# print(elemets)
+
+# # o/p ==> ('hello', 2, [True, 30, 'hai', 'Python'])
+
+# /================================================================================================================================
+
+# elemets[2].insert(2,"Python")
+
+# print(elemets)
+
+# #  o/p ==> ('hello', 2, [True, 30, 'Python', 'hai', 'Python'])
+
+# # elemets[2].insert(0, (92,3,4))
+
+# # print(elemets)
+
+# #  o/p ==> ('hello', 2, [(92, 3, 4), True, 30, 'Python', 'hai', 'Python'])
+
+elemet = (1)
+
+print(type(elemet))  #class int
+
+lem =(1,)
+
+print(type(lem))  #class tuple
