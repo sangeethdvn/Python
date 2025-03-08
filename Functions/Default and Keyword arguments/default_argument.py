b0df905ed29a9c94fda1d8fda6261c2501ba43f0
P S Sangeeth Devan <123802785+sangeethdvn@users.noreply.github.com>
2025-03-08 11:19:05 +0530

    Add files via upload

---
+++ Functions/Default and Keyword arguments/default_argument.py
@@ -0,0 +1,10 @@
+def details(name,position, place="Mumbai") :
+
+    print(f" Hi {name} you are from {place} and yout position is {position}")
+
+details(name="Sangeeth", position="Trainer")
+
+details(name="Sangeeth", position="Trainer", place= "Thrissur")
+
+
+# default arguments in python are function arguments that will be used if no argumensta are passed to the function callings
---
+++ Functions/Default and Keyword arguments/keyword_arguments.py
@@ -0,0 +1,7 @@
+def details(name, place, position):
+
+    print(f" Hi {name} you are from {place} and yout position is {position}")
+
+details(name="Sangeeth", position="Trainer", place= "Thrissur")
+
+# send arguments with the key = value synatax this way the argumensts order dont matter ==> Keyword arguments
